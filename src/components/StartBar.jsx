0bff0c4e84f867f84830a7d146a14abf4f8b2cca
Nick Bowden <nickbwdn@gmail.com>
2023-03-02 13:52:25 -0800

    adjusted start bar css, added current time, added win 95 logo

--- src/components/StartBar.jsx
+++ src/components/StartBar.jsx
@@ -1,10 +1,20 @@
 import './StartBar.css'
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function StartBar(props) {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+      let timer = setInterval(()=>setDate(new Date()), 1000);
+      return function cleanup() {
+        clearInterval(timer)
+      }
+    })
+
     return (
         <div className='start-bar'>
-            <button className='start-button'>Start</button>
+            <button className='start-button'><img src="src\assets\win95.png" alt="w95 icon" className='win95-icon'/> Start</button>
+            <button className='time-button'>{date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}</button>
         </div>
     );
 }
