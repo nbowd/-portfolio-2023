04ca5961cdda60e4b4cfde8b1e9d0998e6598bc6
Nick Bowden <nickbwdn@gmail.com>
2023-03-02 12:47:25 -0800

    initial commit, created Homepage and StartBar componenents

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,14 @@
+import './Homepage.css'
+import React from 'react';
+import StartBar from './StartBar';
+
+function Homepage() {
+    return (
+        <div className='homepage'>
+            <h1>Hello World!</h1>
+            <StartBar/>
+        </div>
+    );
+}
+
+export default Homepage;
---
+++ src/components/StartBar.jsx
@@ -0,0 +1,12 @@
+import './StartBar.css'
+import React from 'react';
+
+function StartBar(props) {
+    return (
+        <div className='start-bar'>
+            <button className='start-button'>Start</button>
+        </div>
+    );
+}
+
+export default StartBar;
