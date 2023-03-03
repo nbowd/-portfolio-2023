b98b0b8fa6fefba833910f43cbcfa173fa78c800
Nick Bowden <nickbwdn@gmail.com>
2023-03-02 16:24:32 -0800

    added desktop icons, added hover animations, adjusted css

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,7 +8,40 @@ function Homepage() {
 
     return (
         <div className='homepage'>
-            <h1>Hello World!</h1>
+            <div className="homepage-items">
+                <div className="homepage-item">
+                    <div className="item-icon">
+                        <img src="src\assets\mycomputer.png" alt="" />
+                    </div>
+                    <div className="item-title">
+                        My Bio
+                    </div>
+                </div>
+                <div className="homepage-item">
+                    <div className="item-icon">
+                        <img src="src\assets\recycle.png" alt="" />
+                    </div>
+                    <div className="item-title">
+                        Recycle Bin
+                    </div>
+                </div>
+                <div className="homepage-item">
+                    <div className="item-icon">
+                        <img src="src\assets\folder.png" alt="" />
+                    </div>
+                    <div className="item-title">
+                        Projects
+                    </div>
+                </div>
+                <div className="homepage-item">
+                    <div className="item-icon">
+                        <img src="src\assets\minesweeper.png" id='minesweeper-icon' alt="" />
+                    </div>
+                    <div className="item-title">
+                        Minesweeper
+                    </div>
+                </div>
+            </div>
             {openMenu
                 ?
                 null
